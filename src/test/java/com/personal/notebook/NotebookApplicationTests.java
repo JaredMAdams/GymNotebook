1695047c0c3d4b8e6e6f024017e3a3d58ab766ee
JaredMAdams <jaredmarshalladams@gmail.com>
2022-12-19 12:19:18 -0600

    first commit

---
+++ src/test/java/com/personal/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
